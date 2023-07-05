e385572a45eb2bef4b1076f65ff9cfe1c2a59e3d
Carlos Jimenez <carlosmjimenezg@gmail.com>
2023-07-05 08:55:12 -0400

    Se agrego las compuertas logicas

--- code.js
+++ code.js
@@ -1,35 +1,58 @@
 // Compuertas logicas
 function AND(a, b) {
-    return a && b;
+    resultado = a && b
+    console.log(resultado)
 }
     
 function OR(a, b) {
-    return a || b;
+    resultado = a || b;
+    console.log(resultado)
 }
     
 function NOT(a) {
-    return !a;
+    resultado =  !a;
+    console.log(resultado)
 }
     
 function XOR(a, b) {
-    return (a || b) && !(a && b);
+    resultado = (a || b) && !(a && b)
+    console.log(resultado)
 }
 
 
-//guardamos las variables del multiplexor
-function guardar_multiplexor() {
+//guardamos las variables
+function guardar() {
 
     let multiplexor1 = document.getElementById("input_multi1").value
     let multiplexor2 = document.getElementById("input_multi2").value
+    let binario1 = document.getElementById("input_bina1").value
+    let binario2 = document.getElementById("input_bina2").value
+    console.log(binario1)
+    console.log(binario2)
     console.log(multiplexor1)
     console.log(multiplexor2)
 
-    multiplexor(multiplexor1, multiplexor2)
+    multiplexor(multiplexor1, multiplexor2, binario1, binario2)
 }
 
-function multiplexor(multiplexor1, multiplexor2){
+
+//Multiplexor
+function multiplexor(multiplexor1, multiplexor2, binario1, binario2){
+    //Compuerta OR
     if (multiplexor1 === "0" && multiplexor2 === "0" ) {
-        console.log("hola uwu")
+        OR(binario1, binario2)
+
+        //Compuerta AND
+    } else if (multiplexor1 === "1" && multiplexor2 === "0") {
+        AND(binario1, binario2)
+
+        //Compuerta  NOT
+    } else if (multiplexor1 === "1" && multiplexor2 === "1") {
+        NOT(binario1)  
+
+        //Compuerta XOR
+    }  else if (multiplexor1 === "0" && multiplexor2 === "1") {
+        XOR(binario1, binario2)  
     }
 
 }
